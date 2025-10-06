bac4653625805a4f693d5e5eb60811d15f99ac27
Alaye-Dong <AlayeVast@outlook.com>
2025-10-06 14:05:39 +0800

    chore(deps): 添加 @types/node 依赖并更新别名配置
    - 配置 tsconfig.app.json 和 tsconfig.json 中的路径映射
    - 在 vite.config.ts 中添加路径别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
@@ -5,4 +6,9 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './.vitepress/theme'),
+    },
+  },
 })
